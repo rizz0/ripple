e3251062a525d32f3751858ae226634ac440fe83
Bas Moeys <bas@poki.com>
2022-09-02 22:38:41 +0200

    a

--- ripple-effect.js
+++ ripple-effect.js
@@ -205,24 +205,3 @@ const RippleEffect = function (opts) {
     };
     animate();
 };
-
-var liquidImage = function(container, imgUrl) {
-const imgLoader = new Image();
-          imgLoader.addEventListener('load', function() {
-          var rippleDemo = new RippleEffect({
-        parent: container,      
-        texture: imgUrl,
-        intensity: 0.1,
-        strength: 1.5,
-        area: 1,
-        waveSpeed: 0.001,
-        hover: false,
-        speedIn: 1.1,
-        speedOut: 1,
-        easing: 'Expo.easeOut',
-    });
-            container.style.opacity = '1';
-            rippleDemo.start();
-}, false);
-imgLoader.src = imgUrl;
-}
