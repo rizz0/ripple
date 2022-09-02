f6b37eadb88326aadb452f539f2ef0d3fa02f22d
Bas Moeys <bas@poki.com>
2022-09-02 21:49:55 +0200

    merge:

--- ripple-effect.js
+++ ripple-effect.js
@@ -1,9 +1,3 @@
-/*!
- * Package: ripple-effect v1.0.0
- * Author: Essam Abed
- * License: MIT
- */
-
 const RippleEffect = function (opts) {
     let vertex = `
         varying vec2 vUv;
@@ -211,3 +205,24 @@ const RippleEffect = function (opts) {
     };
     animate();
 };
+
+var liquidImage = function(container, imgUrl) {
+const imgLoader = new Image();
+          imgLoader.addEventListener('load', function() {
+          var rippleDemo = new RippleEffect({
+        parent: container,      
+        texture: imgUrl,
+        intensity: 0.1,
+        strength: 1.5,
+        area: 1,
+        waveSpeed: 0.001,
+        hover: false,
+        speedIn: 1.1,
+        speedOut: 1,
+        easing: 'Expo.easeOut',
+    });
+            container.style.opacity = '1';
+            rippleDemo.start();
+}, false);
+imgLoader.src = imgUrl;
+}
